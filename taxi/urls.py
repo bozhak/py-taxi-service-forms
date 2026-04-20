16a7d42cd90d715b01509907d1371e7266734253
Your Name <you@example.com>
2026-04-20 19:58:39 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 
 from .views import (
     index,
-    CarListView,
-    CarDetailView,
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView,
+    CarListView,
+    CarDetailView,
+    CarCreate,
+    CarUpdate,
+    CarDelete,
 )
 
 urlpatterns = [
@@ -16,12 +22,28 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
+    path(
+        "manufacturers/create",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create",
+    ),
+    path(
+        "manufacturers/<int:pk>/update",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update",
+    ),
+    path(
+        "manufacturers/<int:pk>/delete",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete",
+    ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/create/", CarCreate.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", CarUpdate.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", CarDelete.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
 ]
 
 app_name = "taxi"
